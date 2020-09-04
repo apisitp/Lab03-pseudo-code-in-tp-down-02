aecc7516797b395f178b7943ded618fa4846cfdb
apisit pukinghin <apisit pukinghin@LAPTOP-EEV4BUFC>
2020-09-04 20:38:59 +0700

    Add project files.

---
+++ Lab03-pseudo code in tp down/Source.cpp
@@ -0,0 +1,23 @@
+
+#include<stdio.h>
+
+int main()
+{
+	int x=1, n =1,y;
+	
+	for (n < 10, 000;x < n)
+		{
+			if (n % x == 0)
+			{
+				printf("%d" & y);
+			}
+			else
+			{
+				x = x + 1
+			}
+			n++
+		}
+	
+
+	
+}
